48e6d513d20e0a76e529af4e9c2da22f5ebd3df0
markova <tai8nai@gmail.com>
2025-03-16 21:10:44 +0500

    task01_markova

--- 02-java-types/task01/src/com/example/task01/Task01.java
+++ 02-java-types/task01/src/com/example/task01/Task01.java
@@ -3,14 +3,20 @@ package com.example.task01;
 public class Task01 {
 
     public static String solution() {
-        // Возвращаем нужную строку
+
+        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
+
         return "It's alive! It's alive!";
     }
 
     public static void main(String[] args) {
-        // Тестируем метод solution и выводим результат
+        // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
+        // например вот так:
+
         String result = solution();
         System.out.println(result);
+
+
     }
 
 }
