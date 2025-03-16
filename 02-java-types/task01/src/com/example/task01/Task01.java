040fc7b3675e4d2b4230d7aca8ed7c95df1f95bb
markova <tai8nai@gmail.com>
2025-03-16 21:03:58 +0500

    task01_markova

--- 02-java-types/task01/src/com/example/task01/Task01.java
+++ 02-java-types/task01/src/com/example/task01/Task01.java
@@ -3,19 +3,14 @@ package com.example.task01;
 public class Task01 {
 
     public static String solution() {
-
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return "здесь какая-то неправильная строка";
+        // Возвращаем нужную строку
+        return "It's alive! It's alive!";
     }
 
     public static void main(String[] args) {
-        // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
+        // Тестируем метод solution и выводим результат
         String result = solution();
         System.out.println(result);
-         */
     }
 
 }
